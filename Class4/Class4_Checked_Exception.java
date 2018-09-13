6826308a292cb6a16240374dab9abb29f74ea779
LawrenceKhan <32395268+LawrenceKhan@users.noreply.github.com>
2018-09-13 19:42:21 -0400

    Updated Class4
    Finished coding class 4 to read and process each line and then to print a computed data

--- Class4/Class4_Checked_Exception.java
+++ Class4/Class4_Checked_Exception.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package Class4;
 
 /**
@@ -20,24 +25,45 @@ public class Class4 {
             FileReader filereader = new FileReader(file);
             BufferedReader buffread = new BufferedReader(filereader);
             String line = "";
+
             while ((line = buffread.readLine()) != null) {
 //                System.out.println(line); 
 //                System.out.println(line.substring(11, 19));
                 int locationOfFirstComma = line.indexOf(",");
                 int locationOfSecondComma = line.indexOf(",", locationOfFirstComma + 1);
-//                System.out.println("First Comma: "+locationOfFirstComma + " Second Comma: "+locationOfSecondComma);
-
-                //store the substring in to a variable. 
-                String open = line.substring(locationOfFirstComma + 1, locationOfSecondComma);
-                double openValue;
+                int locationOfThirdComma = line.indexOf(",", locationOfSecondComma + 1);
+                int locationOfFourthComma = line.indexOf(",", locationOfThirdComma + 1);
+                int locationOfFifthComma = line.indexOf(",", locationOfFourthComma + 1);
+                int locationOfSixthComma = line.indexOf(",", locationOfFifthComma + 1);                
+                
+                String date = line.substring(0, locationOfFirstComma);                
+                                
+                double open;
+                double high;                
+                double low;                
+                double close;                
+                double adjClose;                
+                int volume;                
                 try {
-                    openValue = Double.parseDouble(open);
+                    open = Double.parseDouble(line.substring(locationOfFirstComma + 1, locationOfSecondComma));
+                    high = Double.parseDouble(line.substring(locationOfSecondComma + 1, locationOfThirdComma));
+                    low = Double.parseDouble(line.substring(locationOfThirdComma + 1, locationOfFourthComma));
+                    close = Double.parseDouble(line.substring(locationOfFourthComma + 1, locationOfFifthComma));
+                    adjClose = Double.parseDouble(line.substring(locationOfFifthComma + 1, locationOfSixthComma));
+                    volume = Integer.parseInt(line.substring(locationOfSixthComma+1));
                 } catch (NumberFormatException nb) {
 //                    nb.printStackTrace();
                     System.out.println(nb.getLocalizedMessage());
-                    openValue = 0;
+                    open = 1;
+                    high = 1;
+                    low=1;
+                    close=1;
+                    adjClose=1;
+                    volume=1;                   
                 }
-                System.out.println(openValue * 2);
+                double avgPrice = (open+close+high+low)/4;
+                System.out.printf("Date:%s,Open:%f,High:%f,Low:%f,Close:%f,AdjClose:%f,Volume:%,d,Dollar Volume:%,f,Average Price:%f\n",
+                        date,open,high,low,close,adjClose,volume,volume*close,avgPrice);
             }
 
         } //       catch(FileNotFoundException fn){
