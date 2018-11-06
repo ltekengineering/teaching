c5f282889816d040ff69e95538e4adedcc3fdde3
LawrenceKhan <32395268+LawrenceKhan@users.noreply.github.com>
2018-11-06 16:54:27 -0500

    Update Feline.java

--- Class16/Felines/Feline.java
+++ Class16/Felines/Feline.java
@@ -3,7 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Class16;
+package Class16.Feline;
+
+import Class16.Animals;
 
 /**
  *
