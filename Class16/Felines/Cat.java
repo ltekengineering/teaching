939746a2f5463c69fe88c5bac0730f392ee69135
LawrenceKhan <32395268+LawrenceKhan@users.noreply.github.com>
2018-11-06 16:54:16 -0500

    Update Cat.java

--- Class16/Felines/Cat.java
+++ Class16/Felines/Cat.java
@@ -3,14 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Class16;
+package Class16.Feline;
+
+import Class16.Feline.Feline;
 
 /**
  *
  * @author lkhan
  */
 public class Cat extends Feline{
-    private String size;
+    
 
     public Cat(String size) {
         this.size = size;
