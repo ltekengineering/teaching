9a701ad1aca2624afd2ce111775ac652fcc23698
LawrenceKhan <32395268+LawrenceKhan@users.noreply.github.com>
2018-11-06 16:52:45 -0500

    Create Anaconda.java

---
+++ Class16/Reptile/Anaconda.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Class16.Reptile;
+
+/**
+ *
+ * @author lkhan
+ */
+public class Anaconda extends Reptile{
+
+    public Anaconda() {
+    }
+    @Override
+    public void eat(){
+        System.out.println("I am in the "+this.getClass().getSimpleName()+" class");
+        System.out.println("I am a carnivores");
+        System.out.println("I constrict my prey");
+    }    
+}
