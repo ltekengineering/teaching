f75994d3b6f36c9f99c1d4beb1e546292ca5eb30
lakhan <lakhan@906-00.bmcc.cuny.edu>
2018-11-27 19:38:30 -0500

    Adding Class 21 package with solution to Exam question #22

---
+++ src/Class21/Car.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Class21;
+
+/**
+ *
+ * @author lakhan
+ */
+public abstract class Car {
+    protected String model;
+    public Car(String model){
+        this.model = model;
+    }    
+    protected abstract void laneKeepingMechanism();
+    protected abstract void collisionPrevention();
+    protected abstract void autoParking();
+}
