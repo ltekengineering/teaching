7f1e18ef9db91b0a82a2ce8130621e71eee8133a
lakhan <lakhan@906-00.bmcc.cuny.edu>
2018-11-27 19:45:22 -0500

    Adding Class 21 package with solution to Exam question #22

---
+++ src/Class21/MercedezBenz.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Class21;
+
+/**
+ *
+ * @author lakhan
+ */
+public class MercedezBenz extends Car{
+    public MercedezBenz(String model){
+        super(model);
+    }
+    @Override
+    protected void laneKeepingMechanism(){
+        if(this.model.equals("Camry")){
+            System.out.println(this.model + " has lane keeping feature");
+        }else{
+            System.out.println(this.model + " does not have lane keeping feature");
+        }
+    }
+    @Override
+    protected void collisionPrevention(){
+        if(this.model.equals("Camry")){
+            System.out.println(this.model + " has collision prevention feature");
+        }else{
+            System.out.println(this.model + " does not have collision prevention feature");
+        }
+    }
+    @Override
+    protected void autoParking(){
+        if(this.model.equals("Camry")){
+            System.out.println(this.model + " has auto parking feature");
+        }else{
+            System.out.println(this.model + " does not have auto parking feature");
+        }
+    }
+}
