e0ce584f0de38fb52ee2a2aaf0ada5b5dc173659
lakhan <lakhan@906-00.bmcc.cuny.edu>
2018-12-06 17:58:37 -0500

    Remote connection to a csv file.

---
+++ src/Class24/RemoteCSV.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Class24;
+
+        
+/**
+ *
+ * @author lakhan
+ */
+public class RemoteCSV {
+
+    
+    
+}
