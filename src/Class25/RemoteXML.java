15b704e8c4dc6628f9eb2bd52e098f04c3c59ee7
lakhan <lakhan@906-00.bmcc.cuny.edu>
2018-12-11 19:01:08 -0500

    XML Parsing done

--- src/Class25/RemoteXML.java
+++ src/Class25/RemoteXML.java
@@ -3,7 +3,6 @@
  * and open the template in the editor.
  */
 package Class25;
-import Class24.*;
 import java.net.URL;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -11,6 +10,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.FileWriter;
 import java.io.BufferedWriter;
+
+//XML related imports
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+//W3C Document Object Model standard or in short DOM
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Element;
+
 
 
 /**
@@ -29,26 +40,50 @@ public class RemoteXML implements Runnable{
     public void run(){
         
         try {
-            
+            //Reader info
             URL url = new URL(this.url);            
-            InputStream inp = url.openStream();
-            InputStreamReader inpr = new InputStreamReader(inp);
-            BufferedReader bfr = new BufferedReader(inpr);
+            InputStream inp = url.openStream();            
+            //Writing mechanism
             FileWriter fw = new FileWriter("C:\\Users\\lakhan\\Downloads\\"+this.url.replace("/","_").replaceAll("[$&+,:;=?@#|]", "_"));
-            BufferedWriter bfw = new BufferedWriter(fw);
+            BufferedWriter bfw = new BufferedWriter(fw);            
+            //XML realted code
+            DocumentBuilderFactory domBuilderFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = domBuilderFactory.newDocumentBuilder();
+            Document doc = documentBuilder.parse(inp);//this is the line that parses the xml file.
+            doc.getDocumentElement().normalize();
             
-            String line;
-            int counter=0;
-            while((line=bfr.readLine())!=null){
-                if(writefile){
-                    bfw.write(line+"\n");
-                }                
-//                System.out.println(Arrays.toString(line.trim().split(",")));
-                counter++;
+            System.out.println("Initial node name: "+doc.getDocumentElement().getNodeName());
+            System.out.println("Initial node type: "+doc.getDocumentElement().getNodeType());
+            System.out.println("Initial node value: "+doc.getDocumentElement().getNodeValue());
+            
+            NodeList nodeList = doc.getElementsByTagName("row");
+            
+            for(int i = 0;i<nodeList.getLength();i++){
+                Node eachRow = nodeList.item(i);
+                
+                NodeList childNodes = eachRow.getChildNodes();
+                String line = "";
+                for (int j = 0; j < childNodes.getLength(); j++) {
+                    Node node  = childNodes.item(j);
+                   line+=  node.getNodeName()+" : "+node.getTextContent()+". ";
+                }
+                System.out.println(line);
+//                System.out.printf("Zipe code: %s. "
+//                        + "Total Population: %s. "
+//                        + "Median Age: %s. "
+//                        + "Total Males: %s."
+//                        + "Total Females: %s."
+//                        + "Total Household: %s."
+//                        + "Average Household Size: %s. \n",eachRow.getFirstChild().getTextContent(),
+//                        eachRow.getFirstChild().getNextSibling().getTextContent(),
+//                        eachRow.getFirstChild().getNextSibling().getNextSibling().getTextContent(),
+//                        eachRow.getFirstChild().getNextSibling().getNextSibling().getNextSibling().getTextContent(),
+//                        eachRow.getFirstChild().getNextSibling().getNextSibling().getNextSibling().getNextSibling().getTextContent(),
+//                        eachRow.getFirstChild().getNextSibling().getNextSibling().getNextSibling().getNextSibling().getNextSibling().getTextContent(),
+//                        eachRow.getFirstChild().getNextSibling().getNextSibling().getNextSibling().getNextSibling().getNextSibling().getNextSibling().getTextContent());                
             }
-            bfw.close();
-            System.out.printf("There are %d lines in the file\n",counter);
-        } catch (IOException e) {
+            
+        } catch (Exception e) {
             e.printStackTrace();
         }
         
